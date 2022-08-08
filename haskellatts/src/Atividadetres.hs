2948916ce6b1ff00344456b93ea229a3342d7927
Anna Victoria Feliciana <anna.feliciana@upe.br>
2022-08-07 21:04:15 -0300

    atividade três finalizada

--- haskellatts/src/Atividadetres.hs
+++ haskellatts/src/Atividadetres.hs
@@ -1,6 +1,7 @@
 module Atividadetres where
 
 -- questao um
+somaate100quadrado :: Integer
 somaate100quadrado = sum [x * x | x <- [1 .. 100]]
 
 -- questao dois
@@ -45,7 +46,52 @@ perfeitos n = [x | x <- [1 .. n] , x == sum (init (fatores x))]
 --Dica: Procure usar a função concat. 
 
 --concat [[(1,3),(1,4)] [(2,3), (2,4)]]
+concatenarfun :: [(Integer, Integer)]
 concatenarfun = concat  [[(1,y) | y <- [3,4]], [(2,y) | y <- [3,4]]]
 
 --questão oito
--- 
+-- Redefina a função posicoes usando a função buscar, disponível em 
+-- https://emanoelbarreiros.github.io/funcional/haskell-5#a-fun%C3%A7%C3%A3o-zip
+
+
+buscar :: Eq a => a -> [(a,b)] -> [b]
+buscar k xs = [v | (k', v) <- xs, k == k']
+
+
+posicoes :: Eq a => a -> [a] -> [Int]
+posicoes x xs = [i | (x', i) <- zip xs [0 ..], x == x']
+
+-- *Questaooito> posicoes 3 [1,50,5,85,3,8,89]
+-- [4]
+-- *Questaooito> posicoes 3 [1,50,5,85,3,8,89,3,7,7,5,3]
+-- [4,7,11]
+
+-- zip xs [0 ..] retorna uma lista de tuplas (x', i) onde x' é o elemento de xs e i é o índice de xs
+-- zip pega duas listas e combina-as em uma lista de tuplas
+-- avaliação preguiçosa é o processamento incompleto de uma função, onde o haskell determina a parada
+-- da função por indução do contexto. Indução do contexto é o ato do haskell ver a função por completo
+-- e entender que uma parte da função se esgota antes da outra, seja por condicional ou por variaveis.
+
+meuposicoes :: (Eq a, Num b, Enum b) => a -> [a] -> [b]
+meuposicoes x xs = buscar x (zip xs [0 ..]) 
+-- jeito mais simples de fazer o meuposicoes
+
+meuposicoes' :: (Eq a, Num b, Enum b) => a -> [a] -> [b]
+meuposicoes' x xs = buscar x [(a, b) | (a, b) <- zip xs [0 ..]]
+-- outra forma usando compreensão de listas
+
+--questao nove
+-- O produto escalar de duas listas de inteiros xs e ys de tamanho n é dado pelo produto dos inteiros em posições correspondentes:
+-- n-1
+--  E     (xs i * ys i)
+-- i = 0
+-- imagem na pasta
+
+produtoescalar :: [Int] -> [Int] -> Int
+produtoescalar [] _ = 0
+produtoescalar _ [] = 0
+produtoescalar (x:xs) (y:ys) = x * y + produtoescalar xs ys
+
+
+produtoescalar' :: Num a => [a] -> [a] -> a
+produtoescalar' x y = sum [a * b | (a, b) <- zip x y]
